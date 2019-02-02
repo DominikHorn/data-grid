167e6fa979e19d1c4241cc3e0bc88e9ed2995d93
Dominik Horn <horn.dominik101@yahoo.de>
2019-02-02 18:35:01 +0100

    Add manual-test facility
    * npm run manual-test will open a plain html website with the component mounted for testing

---
+++ index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {DataGrid} from "./src/DataGrid";
+
+ReactDOM.render(<DataGrid/>, document.getElementById("root"));
---
+++ src/DataGrid.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class DataGrid extends React.Component<any,any> {
+    render() {
+        return (
+            <div>
+                Dies ist ein Test :)
+            </div>
+        );
+    }
+}
+
+export {DataGrid};
